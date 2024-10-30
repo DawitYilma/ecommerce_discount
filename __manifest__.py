1d8d5df1eae66ecb79386076ede4e1b2ac2783ec
Dawit Yilma <61190946+DawitYilma@users.noreply.github.com>
2024-10-30 17:55:09 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Ecommerce Discount',
+    'version' : '1.1',
+    'summary': 'Ecommerce Discount',
+    'sequence': 10,
+    'description': """ """,
+    'category': '',
+    'website': '',
+    'depends': ['website_sale'],
+    'data': [
+        'views/product_template.xml',
+        'views/templates.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'assets': {},
+    'license': 'LGPL-3',
+}
